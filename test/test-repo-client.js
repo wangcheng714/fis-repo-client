6cfb264b2c218eb513b892ff4c8d14992380012e
wangcheng <wangcheng714@gmail.com>
2013-09-02 15:56:13 +0800

    完成adduser开发支持

--- test/test-repo-client.js
+++ test/test-repo-client.js
@@ -2,7 +2,15 @@
 var RepoClient = require("../fis-repo-client.js"),
     client = new RepoClient();
 
-client.adduser("wangcheng", "890714", function(error, message){
+//client.adduser("wangcheng", "890714", function(error, message){
+//    if(error){
+//        console.log(error);
+//    }else{
+//        console.log(message);
+//    }
+//});
+
+client.adduser("wangcheng723", "890714", function(error, message){
     if(error){
         console.log(error);
     }else{
