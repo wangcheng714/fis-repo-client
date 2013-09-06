3b95cfaa534803e577e9063891b799a204e7b329
wangcheng <wangcheng714@gmail.com>
2013-09-06 17:22:01 +0800

    完成install功能开发

--- test/test-install.js
+++ test/test-install.js
@@ -3,16 +3,37 @@
 var RepoClient = require("../fis-repo-client.js"),
     client = new RepoClient();
 
-var dir = __dirname,
-    pkg = {
-        name : "fis-cloud-app-ppt",
-        version : "0.0.2"
-    },
-    options = {
 
-    };
 
-client.install(dir, pkg, options, function(error, content){
-    console.log(error);
-    console.log(content);
-});
+function installByPackageName(){
+    var dir = __dirname,
+        pkg = {
+            name : "fis-cloud-app-ppt",
+            version : "0.0.2"
+        },
+        options = {
+            deps : true
+        };
+
+    client.install(dir, pkg, null, options, function(error, content){
+        console.log(error);
+        console.log(content);
+    });
+}
+
+function installByPackageJson(){
+    var dir = __dirname,
+        file = dir + "/package.json",
+        pkg = null,
+        options = {
+            deps : true
+        };
+
+    client.install(dir, pkg, file, options, function(error, content){
+        console.log(error);
+        console.log(content);
+    });
+}
+
+installByPackageJson();
+
