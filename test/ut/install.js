82a277c9f50ddf51d314132308bc6d7b09251a3c
tianlili <tianlili@baidu.com>
2013-10-25 11:09:53 +0800

    [change]补充脚手架的用例

--- test/ut/install.js
+++ test/ut/install.js
@@ -9,12 +9,12 @@ describe("install", function(){
     it("not exist pkg", function(done){
         var dir = './install/1',
             pkg = {
-                name : "a",
+                name : "atest",
                 version: "latest"
             };
 
         client.install(dir, pkg, null, function(error, message){
-            expect(error.toString()).to.equal("Component [a] not found!");
+            expect(error.toString()).to.equal("Component [atest] not found!");
             done();
         });
     });
@@ -27,6 +27,7 @@ describe("install", function(){
             };
 
         client.install(dir, pkg, null, function(error, message){
+            console.log(error);
             expect(error.toString()).to.equal("Component [smart-cov@0.0.10] not found!");
             done();
         });
@@ -44,6 +45,27 @@ describe("install", function(){
                 var file = fs.readFileSync(dir + "/smart-cov/package.json", "utf8");
                 expect(JSON.parse(file).version).to.equal("0.0.1");
                 done();
+            });
+        });
+    });
+
+    it("scaffold", function(done){
+        var dir = './install/3',
+            pkg = {
+                name : "smart-cov",
+                version: "0.0.3"
+            };
+        client.unpublish(pkg, {}, function(error, message){
+            client.publish("./publish/7", {}, function(error, message){
+                expect(message).to.equal("\"Publish component [smart-cov@0.0.3] success!\"");
+                rmTree(dir + "/smart-cov", function(){
+                    client.install(dir, pkg, null, function(error, message){
+                        expect(message).to.equal("Component [smart-cov@0.0.3] Install Success.");
+                        var file = fs.readFileSync(dir + "/smart-cov/package.json", "utf8");
+                        expect(JSON.parse(file).version).to.equal("0.0.3");
+                        done();
+                    });
+                });
             });
         });
     });
